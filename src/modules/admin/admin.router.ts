864ee2e9e62ed00ebf3e83ee048bf2094d47cae7
Timur <raxmatovtimurjava@gmail.com>
2023-08-16 16:29:33 +0500

    added get byid admin

--- src/modules/admin/admin.router.ts
+++ src/modules/admin/admin.router.ts
@@ -2,6 +2,7 @@ import { Router } from "express"
 import adminController from "./admin.controller"
 import validate from "../../middlewares/validate"
 import super_admin_check_token from "../../middlewares/super_admin_check_token"
+import check_token from "../../middlewares/check_token"
 
 const router: Router = Router()
 
@@ -9,6 +10,7 @@ const router: Router = Router()
 router.post("/login", validate, adminController.LOGIN)
 router.post("/create/admin", super_admin_check_token, adminController.CREATE_ADMIN)
 router.put("/update/admin/:id", super_admin_check_token, validate , adminController.UPDATE_ADMIN)
+router.get("/admin" , check_token , adminController.GET_ADMIN)
 router.delete("/delete/admin/:id" ,super_admin_check_token , adminController.DELETE)
 
 
